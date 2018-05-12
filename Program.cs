6cee0aaaabc955e9776a005c2a5cc2f4b5c5384e
umesh <umesh.riverbed@gmail.com>
2018-05-12 11:57:32 +0200

    password service

--- Program.cs
+++ Program.cs
@@ -36,99 +36,78 @@ namespace MultiStrokeGestureRecognitionLib
             string ConnectionString = "Server=localhost; Port=5432; User Id=touchy; Password=123456;Database = touchy_data_development";
             string ConnectionString_heroku = "Database=dcbpejtem8e4qu; Server=ec2-54-75-239-237.eu-west-1.compute.amazonaws.com; Port=5432; User Id=pbcgcsyjsmpeds; Password=323743a3eec80c0a49dcee493617af7b94fee458a6a89a671dc3acaad0c3f437; Sslmode=Require;Trust Server Certificate=true";
             NpgsqlConnection connection = new NpgsqlConnection(ConnectionString_heroku);
-            NpgsqlConnection connection_1 = new NpgsqlConnection(ConnectionString_heroku);
             try
             {
                 connection.Open();
-                connection_1.Open();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            NpgsqlCommand waiting = connection_1.CreateCommand();
-            waiting.CommandText = "SELECT * FROM geslogs WHERE verified='0'";
-            NpgsqlDataReader wList = waiting.ExecuteReader();
-            while (wList.Read())
+            NpgsqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM trajectories WHERE user_id=3 AND gesture_id=2";
+            NpgsqlDataReader reader = command.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            var passwordSet = dt.Select("exec_num < 11");
+            foreach (DataRow row in passwordSet)
             {
-                NpgsqlCommand command = connection.CreateCommand();
-                command.CommandText = "SELECT DISTINCT gesture_id FROM trajectories WHERE user_id=" + wList.GetValue(1) + " AND is_password = '1'";
-                NpgsqlDataReader gesture_id = command.ExecuteReader();
-                DataTable dg = new DataTable();
-                dg.Load(gesture_id);
-                var gPid = 0;
-                foreach (DataRow row in dg.Rows)
-                {
-                    gPid = Convert.ToInt32(row["gesture_id"]);
-                }
-                command.CommandText = "SELECT * FROM trajectories WHERE user_id=" + wList.GetValue(1) + " AND gesture_id=" + gPid;
-                NpgsqlDataReader reader = command.ExecuteReader();
-                DataTable dt = new DataTable();
-                dt.Load(reader);
-                var passwordSet = dt.Select("is_password=1");
-                foreach (DataRow row in passwordSet)
-                {
 
-                    string[,] db_points = row["points"] as string[,];
-                    var gestureName = row["user_id"] + "-" + row["gesture_id"] + "-" + row["stroke_seq"];
-                    var user = Convert.ToInt32(row["id"]);
-                    var trace = Convert.ToInt32(row["stroke_seq"]);
-                    var trajectory = new StrokeData(user, trace, db_points);
-                    strokeCollection.Add(new KeyValuePair<String, StrokeData>(gestureName, trajectory));
-                }
+                string[,] db_points = row["points"] as string[,];
+                var gestureName = row["user_id"] + "-" + row["gesture_id"] + "-" + row["stroke_seq"];
+                var user = Convert.ToInt32(row["id"]);
+                var trace = Convert.ToInt32(row["stroke_seq"]);
+                var trajectory = new StrokeData(user, trace, db_points);
+                strokeCollection.Add(new KeyValuePair<String, StrokeData>(gestureName, trajectory));
+            }
 
-                var lookup = strokeCollection.ToLookup(kvp => kvp.Key, kvp => kvp.Value);
-                var keys = lookup.Select(g => g.Key).ToList();
+            var lookup = strokeCollection.ToLookup(kvp => kvp.Key, kvp => kvp.Value);
+            var keys = lookup.Select(g => g.Key).ToList();
 
-                for (int i = 0; i < keys.Count; i++)
+            for (int i = 0; i < keys.Count; i++)
+            {
+                List<StrokeData> fingerCollection = new List<StrokeData>();
+                foreach (StrokeData x in lookup[keys[i]])
                 {
-                    List<StrokeData> fingerCollection = new List<StrokeData>();
-                    foreach (StrokeData x in lookup[keys[i]])
-                    {
-                        fingerCollection.Add(x);
-                    }
-                    cs.trainGesture(keys[i], fingerCollection.Cast<BaseTrajectory>());
+                    fingerCollection.Add(x);
                 }
+                cs.trainGesture(keys[i], fingerCollection.Cast<BaseTrajectory>());
+            }
 
-                var ckeckSet = dt.Select("is_password=2");
-                var result = new double[ckeckSet.Count()];
-                int c = 0;
-                foreach (DataRow row in ckeckSet)
-                {
-                    string[,] db_points = row["points"] as string[,];
-                    var gestureName = row["user_id"] + "-" + row["gesture_id"] + "-" + row["stroke_seq"];
-                    var user = Convert.ToInt32(row["id"]);
-                    var trace = Convert.ToInt32(row["stroke_seq"]);
-                    var trajectory = new StrokeData(user, trace, db_points);
-                    result[c] = cs.getSimilarity(gestureName, trajectory);
-                    c++;
-                }
+            var ckeckSet = dt.Select("exec_num >= 11");
+            var result = new double[ckeckSet.Count()];
+            int c = 0;
+            foreach (DataRow row in ckeckSet)
+            {
+                string[,] db_points = row["points"] as string[,];
+                var gestureName = row["user_id"] + "-" + row["gesture_id"] + "-" + row["stroke_seq"];
+                var user = Convert.ToInt32(row["id"]);
+                var trace = Convert.ToInt32(row["stroke_seq"]);
+                var trajectory = new StrokeData(user, trace, db_points);
+                result[c] = cs.getSimilarity(gestureName, trajectory);
+                c++;
+            }
 
-                bool ok = false;
-                foreach (var prob in result)
+            bool ok = false;
+            foreach (var prob in result)
+            {
+                Console.WriteLine(prob);
+                if (prob <= 0)
                 {
-                    Console.WriteLine(prob);
-                    if (prob <= 0)
-                    {
-                        ok = false;
-                        break;
-                    }
-                    ok = true;
+                    //ok = false;
+                    break;
                 }
-                NpgsqlCommand insertion = connection.CreateCommand();
+                ok = true;
+            }
+            NpgsqlCommand insertion = connection.CreateCommand();
 
-                if (ok)
-                {
-                    Console.WriteLine(" Gesture matches!");
-                    insertion.CommandText = "UPDATE geslogs SET verified=1 WHERE user_id=" + wList.GetValue(1);
-                    insertion.ExecuteNonQuery();
-                }
-                else
-                {
-                    Console.WriteLine(" Gesture does not match!");
-                    insertion.CommandText = "UPDATE geslogs SET verified=2 WHERE user_id=" + wList.GetValue(1);
-                    insertion.ExecuteNonQuery();
-                }
+            if (ok)
+            {
+                Console.WriteLine(" Gesture matches!");
+            }
+            else
+            {
+                Console.WriteLine(" Gesture does not match!");
             }
         }
     }
